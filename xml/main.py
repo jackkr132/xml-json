e80e5b2cfe38d6ae7e1969b17ab76a8fb9a45451
jackkr132 <72446270+jackkr132@users.noreply.github.com>
2020-10-28 17:38:16 +0300

    Add files via upload

---
+++ xml/main.py
@@ -0,0 +1,18 @@
+import xml.etree.ElementTree as ET
+parser = ET.XMLParser(encoding="utf-8")
+tree = ET.parse("amp2.xml", parser)
+root = tree.getroot()
+from collections import Counter
+def get(filename: str, top_size: int, min_word_len: int) -> str:
+    all_words = []
+    open_file = root
+    find_file = open_file.findall("channel/item")
+    for article in find_file:
+        words = article.find("description").text.lower().split()
+        for word in words:
+            if len(word) > min_word_len:
+                all_words.append(word)
+    counter = Counter(all_words)
+    sorted_list = counter.most_common(10)
+    return f"топ {top_size} самых часто встречаемых слов:{sorted_list}"
+print(get("amp2.xml", 10, 6))
